532dc0c99b870ac302c83e7bcb31b081ad04a9a8
Muhammad Sahil <muhammadsahil2111e@aptechsite.net>
2024-12-14 15:44:15 +0500

    Models & Seeder User

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,23 +1,72 @@
 const { default: mongoose } = require("mongoose");
 
-const ContactSchema = new mongoose.Schema({
-  ip: {
-    type: String,
+const UserSchema = new mongoose.Schema({
+  role: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Role",
+    required: true,
   },
-  full_name: {
+  first_name: {
     type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
   },
   email: {
     type: String,
+    required: true,
   },
-  subject: {
+  password: {
+    type: String,
+    required: true,
+  },
+  date_of_birth: {
+    type: Date,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["Male", "Female"],
+    required: true,
+  },
+  phone_number: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  profile: {
+    type: String,
+    required: true,
+  },
+  ip: {
     type: String,
   },
-  message: {
-    type: String,
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
+  currentPlan: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "SubscriptionPlan",
   },
+  planExpiryDate: {
+    type: Date,
+  },
+  paymentStatus: {
+    type: String,
+    enum: ["Pending", "Completed", "Failed"],
+    default: "Pending",
+  },
+  stripeSessionId: {
+    type: String,
+    default: "",
+  },
+  hasUsedFreePlan: { type: Boolean, default: false },
 });
 
-ContactSchema.set("timestamps", true);
+UserSchema.set("timestamps", true);
 
-module.exports = mongoose.model("Contact", ContactSchema);
+module.exports = mongoose.model("User", UserSchema);
